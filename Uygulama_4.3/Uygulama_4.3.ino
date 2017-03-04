97cfab244bd613056f77494f2f502f142a4677da
Mehmet Kemal <mkemalsari@yahoo.com>
2017-03-04 12:26:52 +0200

    Add files via upload

---
+++ Uygulama_4.3/Uygulama_4.3.ino
@@ -0,0 +1,23 @@
+/*
+------------------------------------------------
+Program No: 4.3
+Program Adı: Butona basıldığında LED’i yak
+Kullanılan Geliştirme Kiti: Arduino Uno
+Kullanılan Eklenti: -
+Web Adres: www.ArduinoUygulamalari.com
+-------------------------------------------------
+*/
+void setup() {
+  // Hangi dijital pinleri output olarak seçeceğimizi belirtiyoruz.
+  pinMode(13, OUTPUT);
+  pinMode(9, INPUT);
+}
+void loop() {
+  int butondegeri = digitalRead(9);
+  if (butondegeri == 0) {
+    digitalWrite(13, HIGH);
+  } else {
+    digitalWrite(13, LOW);
+  }
+}
+
