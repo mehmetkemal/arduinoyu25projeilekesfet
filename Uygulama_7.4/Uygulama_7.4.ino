b2f35cb7bb2023176160e02916d2adbb913aa3e0
Mehmet Kemal <mkemalsari@yahoo.com>
2017-03-04 13:08:10 +0200

    Add files via upload

---
+++ Uygulama_7.4/Uygulama_7.4.ino
@@ -0,0 +1,28 @@
+/*
+Program No: 7.4
+Program Adı: step motor kontrolu
+Kullanılan Arduino Modeli: Arduino Uno
+Motor Sürücü entegresi:ULN2003
+Web Adres: www.arduinouygulamalari.com
+-------------------------------------------------
+*/
+#include <Stepper.h>
+#include <Stepper.h>
+const int stepsPerRevolution = 200; // step motorun 1 tam turunu 200 adımda döner
+Stepper myStepper(stepsPerRevolution, 8, 9, 10, 11); // step motorun bağlantı uçları
+void setup() {
+  // dönüş hızı 60 rpm
+  myStepper.setSpeed(60);
+  Serial.begin(9600);
+}
+void loop() {
+  // 200 adım dön.
+  Serial.println("saat yönünde dönüş");
+  myStepper.step(stepsPerRevolution);
+  delay(500);
+  // step one revolution in the other direction:
+  Serial.println("saat yönü tersine dönüş");
+  myStepper.step(-stepsPerRevolution);
+  delay(500);
+}
+
