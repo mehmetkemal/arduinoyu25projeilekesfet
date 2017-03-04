9c504022fc52919a0a009620fcc8e43625da0f29
Mehmet Kemal <mkemalsari@yahoo.com>
2017-03-04 12:18:30 +0200

    Add files via upload

---
+++ Uygulama_2.3/Uygulama_2.3.ino
@@ -0,0 +1,41 @@
+/*
+  ------------------------------------------------
+  Program No: 2.3
+  Program Adı: Döngülerin Kullanımı
+  Kullanılan Geliştirme Kiti: Arduino Uno
+  Web Adres: www.arduinouygulamalari.com
+  -------------------------------------------------
+*/
+
+
+
+int donguSayisi;
+int Sayi1, Sayi2;
+
+void setup() {
+  Serial.begin(9600); // seri monitörü kullanmak için bağlantı hızı seçilir
+  Serial.println("Arduino Uygulamalari");
+
+  Sayi1 = 50;
+  Sayi2 = 100;
+}
+
+void loop() {
+
+  for (int i = 0; i < 10; i++)
+  {
+    Serial.print("i =");
+    Serial.println(i);
+  }
+
+  while (donguSayisi < 10)
+  {
+    Serial.print("Dongu Sayisi:");
+    Serial.println(donguSayisi);
+    donguSayisi++; // donguSayisi'ni bir artır
+  }
+
+  while (1) {}
+
+}
+
